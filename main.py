aa5259b2efb716a4b246983cccbb8955cf3e2723
endra030 <narendraannabathula@VIRA.local>
2025-04-12 09:26:11 -0700

    completed CH2:L6

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import word_count
+from stats import char_count
 def get_book_text(file_name):
     file_content = ""
     with open(file_name) as f:
@@ -10,6 +11,8 @@ def main():
     content = get_book_text("books/frankenstein.txt")
     num_words = word_count(content)
     print(f"{num_words} words found in the document")
+    #content = "The Project Gutenberg eBook of Frankenstein; Or, The Modern Prometheus"
+    print(char_count(content))
 
 main()
         
--- stats.py
+++ stats.py
@@ -1,2 +1,20 @@
 def word_count(content):
     return len(content.split())
+
+def char_count(content):
+    character_count = {}
+    for c in content:
+        #print(f"w: {c}")
+        if c.isalpha():
+            if c.lower() in character_count:
+                character_count[c.lower()] =+ character_count[c.lower()]+1
+            else:
+                character_count[c.lower()] = 1
+        else:
+            if c in character_count:
+                character_count[c] =+ character_count[c]+1
+            else:
+                character_count[c] = 1
+            character_count[c] =+ character_count[c]+1
+        
+    return character_count
